7ee478387e0cf578ebaf4b6d2cb3d8634d50cc94
Ahmed -El-Seginy <ahmed0elseginy@gmail.com>
2025-01-05 21:38:17 +0200

    1

--- src/main/java/com/usermanagement/ZagSystemApplication.java
+++ src/main/java/com/usermanagement/ZagSystemApplication.java
@@ -4,8 +4,6 @@ package com.usermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Scanner;
-
 @SpringBootApplication
 public class ZagSystemApplication {
 
---
+++ src/main/java/com/usermanagement/service/implementation/UserImplementation.java
@@ -0,0 +1,4 @@
+package com.usermanagement.service.implementation;
+
+public class UserImplementation {
+}
